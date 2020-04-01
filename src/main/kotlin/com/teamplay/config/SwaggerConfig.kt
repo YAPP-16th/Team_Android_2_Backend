4ac623c93933d923911f0058acd6e2bfba757864
minseong choi <cms3136@gmail.com>
2020-04-02 00:24:45 +0900

    spring boot 초기셋팅

---
+++ src/main/kotlin/com/teamplay/config/SwaggerConfig.kt
@@ -0,0 +1,24 @@
+package com.teamplay.config
+
+
+import io.swagger.annotations.ApiOperation
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation::class.java))
+                .build()
+    }
+}
---
+++ src/main/kotlin/com/teamplay/controller/TestController.kt
@@ -0,0 +1,21 @@
+package com.teamplay.controller
+
+import io.swagger.annotations.ApiOperation
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/user")
+class TestController {
+
+    @ApiOperation(value = "테스트 API")
+    @PostMapping("/test/{testId}")
+    @ResponseStatus(HttpStatus.OK)
+    fun checkDuplicationUserId(@Valid @PathVariable testId: String) : String{
+
+        return "테스트번호 $testId"
+    }
+
+
+}
