81a5f5a32cfca43079127342d153d112563dfa63
minseong choi <cms3136@gmail.com>
2020-04-02 00:27:08 +0900

    spring boot 초기셋팅

---
+++ src/main/kotlin/com/teamplay/ApisApplication.kt
@@ -0,0 +1,11 @@
+package com.teamplay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamPlayApplication
+
+fun main(args: Array<String>) {
+	runApplication<TeamPlayApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamplay/apis/ApisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamplay.apis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
